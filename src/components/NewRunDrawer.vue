26bea85d3cf19e9ba2c0b7c378835a8b6c115305
liugz17 <liugz17@mails.tsinghua.edu.cn>
2022-07-01 16:12:19 +0800

    decoys

--- src/components/NewRunDrawer.vue
+++ src/components/NewRunDrawer.vue
@@ -9,6 +9,8 @@
   >
     <div class="demo-drawer__content">
       <h3>Create New Run</h3>
+      <el-tabs v-model="activeName">
+    <el-tab-pane label="Beginner" name="first">
       <el-form :model="form">
         <el-form-item label="Run Name" :label-width="formLabelWidth">
           <el-input v-model="form.key" autocomplete="off"></el-input>
@@ -18,7 +20,57 @@
             v-model="form.fips"
             placeholder="Please select simulation area"
           >
-            <el-option label="Allegheny County, PA" value=42003></el-option>
+            <el-option label="Allegheny County, PA" value="42003"></el-option>
+            <el-option label="Akhiok County, CA" value="beijing"></el-option>
+            <el-option label="Bethel County, CA" value="bejing"></el-option>
+            <el-option label="Hillsborough County, FL" value="bjing"></el-option>
+            <el-option label="Touisset County, MA" value="beijg"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="Duration" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.days"
+            placeholder="100"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <!-- <el-form-item label="Start Date"  :label-width="formLabelWidth">
+            <el-input v-model="form.name" placeholder="2012-06-01" suffix-icon="el-icon-date" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Number of Diseases"  :label-width="formLabelWidth">
+             <el-input-number
+                v-model="num"
+                @change="handleChange"
+                :min="1"
+                :max="10"
+            ></el-input-number> -->
+        <!-- </el-form-item> -->
+        <el-form-item label="Transmissibility" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.influenza_transmissibility"
+            placeholder="1.0"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Enable Masks">
+          <el-switch v-model="form.enable_travel"> </el-switch>
+        </el-form-item>
+        <el-form-item label="Enable Isolation">
+          <el-switch v-model="form.enable_travel"> </el-switch>
+        </el-form-item>
+      </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="Advanced" name="second">
+          <el-form :model="form">
+        <el-form-item label="Run Name" :label-width="formLabelWidth">
+          <el-input v-model="form.key" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Location" :label-width="formLabelWidth">
+          <el-select
+            v-model="form.fips"
+            placeholder="Please select simulation area"
+          >
+            <el-option label="Allegheny County, PA" value="42003"></el-option>
             <!-- <el-option label="Akhiok County, CA" value="beijing"></el-option>
             <el-option label="Bethel County, CA" value="bejing"></el-option>
             <el-option label="Hillsborough County, FL" value="bjing"></el-option>
@@ -51,7 +103,120 @@
           ></el-input>
         </el-form-item>
         <el-form-item label="School Closure Policies">
-          <el-radio-group v-model="form.closure_policy" >
+          <el-radio-group v-model="form.closure_policy">
+            <el-radio label="none"></el-radio>
+            <el-radio label="global"></el-radio>
+            <el-radio label="individual"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item
+          label="School Closure Duration"
+          :label-width="formLabelWidth"
+        >
+          <el-input-number
+            v-model="form.closure_duration"
+            @change="handleChange"
+            :min="1"
+            :max="100"
+          ></el-input-number>
+        </el-form-item>
+        <el-form-item label="Enable Isolation">
+          <el-switch v-model="form.enable_travel"> </el-switch>
+        </el-form-item>
+        <el-form-item label="Isolation Cover Rate" :label-width="formLabelWidth">
+          <el-input-number
+            v-model="form.min_travel_distance"
+            @change="handleChange"
+            :min="100"
+            :max="200"
+          ></el-input-number>
+        </el-form-item>
+        <el-form-item label="Enable Masks">
+          <el-switch v-model="form.enable_travel"> </el-switch>
+        </el-form-item>
+        <el-form-item label="Masks Protection Rate" :label-width="formLabelWidth">
+          <el-input-number
+            v-model="form.min_travel_distance"
+            @change="handleChange"
+            :min="100"
+            :max="200"
+          ></el-input-number>
+          
+        </el-form-item>
+        <el-form-item label="Masked Type">
+          <el-radio-group v-model="form.masked_type">
+            <el-radio label="infector"></el-radio>
+            <el-radio label="infectee"></el-radio>
+            <el-radio label="both"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="Isolation Duration" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.closure_duration"
+            placeholder="7"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Isolation Delay" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.closure_duration"
+            placeholder="1"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Mask Duration" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.closure_duration"
+            placeholder="7"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="Master" name="third">
+        <el-form :model="form">
+        <el-form-item label="Run Name" :label-width="formLabelWidth">
+          <el-input v-model="form.key" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Location" :label-width="formLabelWidth">
+          <el-select
+            v-model="form.fips"
+            placeholder="Please select simulation area"
+          >
+            <el-option label="Allegheny County, PA" value="42003"></el-option>
+            <!-- <el-option label="Akhiok County, CA" value="beijing"></el-option>
+            <el-option label="Bethel County, CA" value="bejing"></el-option>
+            <el-option label="Hillsborough County, FL" value="bjing"></el-option>
+            <el-option label="Touisset County, MA" value="beijg"></el-option> -->
+          </el-select>
+        </el-form-item>
+        <el-form-item label="Duration" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.days"
+            placeholder="100"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <!-- <el-form-item label="Start Date"  :label-width="formLabelWidth">
+            <el-input v-model="form.name" placeholder="2012-06-01" suffix-icon="el-icon-date" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Number of Diseases"  :label-width="formLabelWidth">
+             <el-input-number
+                v-model="num"
+                @change="handleChange"
+                :min="1"
+                :max="10"
+            ></el-input-number> -->
+        <!-- </el-form-item> -->
+        <el-form-item label="Transmissibility" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.influenza_transmissibility"
+            placeholder="1.0"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="School Closure Policies">
+          <el-radio-group v-model="form.closure_policy">
             <el-radio label="none"></el-radio>
             <el-radio label="global"></el-radio>
             <el-radio label="individual"></el-radio>
@@ -81,10 +246,7 @@
         <el-form-item label="Enable School Summer Break">
           <el-switch v-model="form.school_summer_schedule"> </el-switch>
         </el-form-item>
-        <el-form-item
-          label="Office Size"
-          :label-width="formLabelWidth"
-        >
+        <el-form-item label="Office Size" :label-width="formLabelWidth">
           <el-input-number
             v-model="form.office_size"
             @change="handleChange"
@@ -95,10 +257,7 @@
         <el-form-item label="Enable Travel">
           <el-switch v-model="form.enable_travel"> </el-switch>
         </el-form-item>
-        <el-form-item
-          label="Travel Distance Min"
-          :label-width="formLabelWidth"
-        >
+        <el-form-item label="Travel Distance Min" :label-width="formLabelWidth">
           <el-input-number
             v-model="form.min_travel_distance"
             @change="handleChange"
@@ -106,21 +265,84 @@
             :max="200"
           ></el-input-number>
         </el-form-item>
+        <el-form-item label="Enable Vaccine">
+          <el-switch v-model="form.school_summer_schedule"> </el-switch>
+        </el-form-item>
+        <el-form-item label="Vaccine Num" :label-width="formLabelWidth">
+          <el-input-number
+            v-model="form.office_size"
+            @change="handleChange"
+            :min="5"
+            :max="200"
+          ></el-input-number>
+        </el-form-item>
+        <el-form-item label="Enable Isolation">
+          <el-switch v-model="form.enable_travel"> </el-switch>
+        </el-form-item>
+        <el-form-item label="Isolation Cover Rate" :label-width="formLabelWidth">
+          <el-input-number
+            v-model="form.min_travel_distance"
+            @change="handleChange"
+            :min="100"
+            :max="200"
+          ></el-input-number>
+        </el-form-item>
+        <el-form-item label="Enable Masks">
+          <el-switch v-model="form.enable_travel"> </el-switch>
+        </el-form-item>
+        <el-form-item label="Masks Protection Rate" :label-width="formLabelWidth">
+          <el-input-number
+            v-model="form.min_travel_distance"
+            @change="handleChange"
+            :min="100"
+            :max="200"
+          ></el-input-number>
+        </el-form-item>
+        <el-form-item label="Masked Type">
+          <el-radio-group v-model="form.masked_type">
+            <el-radio label="infector"></el-radio>
+            <el-radio label="infectee"></el-radio>
+            <el-radio label="both"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="Isolation Duration" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.closure_duration"
+            placeholder="7"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Isolation Delay" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.closure_duration"
+            placeholder="1"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Mask Duration" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.closure_duration"
+            placeholder="7"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
       </el-form>
-      <div class="demo-drawer__footer">
+      
+      </el-tab-pane>
+    </el-tabs>
+    <div class="demo-drawer__footer">
         <!-- <el-button @click="cancelForm">Cancel</el-button> -->
-        <el-button
-          type="primary"
-          @click="logger"
-          :loading="loading"
-          >{{ loading ? "Submitting ..." : "Submit" }}</el-button
-        >
+        <el-button type="primary" @click="submit" :loading="loading">{{
+          loading ? "Submitting ..." : "Submit"
+        }}</el-button>
       </div>
     </div>
+    
   </el-drawer>
 </template>
 
 <script>
+import { getCurrentInstance } from "vue";
 export default {
   name: "NewRunDrawer",
   props: {
@@ -131,6 +353,7 @@ export default {
     },
   },
   data() {
+    const { proxy } = getCurrentInstance();
     return {
       // 是否打开
       drawer: false,
@@ -147,12 +370,19 @@ export default {
         office_size: 50,
         enable_travel: 0,
         min_travel_distance: 100,
+        masked_type: "infector",
       },
       formLabelWidth: "80px",
       timer: null,
-
-      
+      proxy,
+      runs_listed: [],
+      activeName: "first",
     };
+  },
+  mounted() {
+    this.proxy.$axios
+      .get(this.proxy.$backend + "/runs/listed")
+      .then((response) => (this.runs_listed = response.data));
   },
   methods: {
     cancelForm() {
@@ -160,9 +390,25 @@ export default {
       this.drawer = false;
       clearTimeout(this.timer);
     },
-    logger() {
-        console.log(this.form)
-    }
+    notDuplicated() {
+      for (var key in this.runs_listed) {
+        var item = this.runs_listed[key]["key"];
+        if (item == this.form.key) {
+          return false;
+        }
+      }
+      return true;
+    },
+    submit() {
+      if (this.form.key != "" && this.form.fips != "" && this.notDuplicated()) {
+        console.log(this.form);
+        this.proxy.$axios
+          .post(this.proxy.$backend + "/addrun/", this.form)
+          .then((response) => console.log(response));
+      } else {
+        console.log("Parameters incorrect!");
+      }
+    },
   },
 };
 </script>
--- src/components/RunPageInfo.vue
+++ src/components/RunPageInfo.vue
@@ -8,6 +8,50 @@
                     <el-table-column prop="value" label="Value" width="200"> </el-table-column>
                 </el-table>
             </el-card>
+            <el-card>
+                <h3>User Config</h3>
+                <el-table :data="UserConfig" style="width: 100%" max-height="400">
+                    <el-table-column prop="key" label="Key" width="200"> </el-table-column>
+                    <el-table-column prop="value" label="Value" width="200"> </el-table-column>
+                </el-table>
+            </el-card>
+        </el-col>
+        <el-col :span="7">
+            <el-card>
+                <h3>Run Data</h3>
+                <el-table :data="RunData" style="width: 100%" max-height="250">
+                    <el-table-column prop="key" label="Key" width="200"> </el-table-column>
+                    <el-table-column prop="value" label="Value" width="200"> </el-table-column>
+                </el-table>
+            </el-card>
+            <el-card>
+                <h3>Statistics</h3>
+                <el-table :data="Statistics" style="width: 100%" max-height="400">
+                    <el-table-column prop="key" label="Key" width="200"> </el-table-column>
+                    <el-table-column prop="value" label="Value" width="200"> </el-table-column>
+                </el-table>
+            </el-card>
+        </el-col>
+        <el-col :span="7">
+            <el-card>
+                <h3>Full Config</h3>
+                <el-table :data="UserConfig" style="width: 100%" max-height="400">
+                    <el-table-column prop="key" label="Key" width="200"> </el-table-column>
+                    <el-table-column prop="value" label="Value" width="200"> </el-table-column>
+                </el-table>
+                <el-table :data="RunData" style="width: 100%" max-height="250">
+                    <el-table-column prop="key" label="Key" width="200"> </el-table-column>
+                    <el-table-column prop="value" label="Value" width="200"> </el-table-column>
+                </el-table>
+                <el-table :data="Statistics" style="width: 100%" max-height="250">
+                    <el-table-column prop="key" label="Key" width="200"> </el-table-column>
+                    <el-table-column prop="value" label="Value" width="200"> </el-table-column>
+                </el-table>
+                <el-table :data="MetaData" style="width: 100%" max-height="400">
+                    <el-table-column prop="key" label="Key" width="200"> </el-table-column>
+                    <el-table-column prop="value" label="Value" width="200"> </el-table-column>
+                </el-table>
+            </el-card>
         </el-col>
     </el-row>
 </template> 
@@ -26,12 +70,79 @@ export default {
       return {
         MetaData: [
         ],
+        RunData: [
+        ],
+        Statistics: [
+          {
+            key:"RunTime",value:342
+          },
+          {
+            key:"Peak_CPU",value:97
+          },
+          {
+            key:"Peak_Memory",value:234623423
+          },
+          {
+            key:"Seed",value:42
+          },
+          {
+            key:"Functions",value:684
+          },
+          {
+            key:"Num_CPU",value:4
+          },
+          {
+            key:"Num_GPU",value:2
+          },
+          {
+            key:"Times",value:3
+          },
+          {
+            key:"Duplicate",value:0
+          },
+        ],
+        UserConfig: [
+          {
+            key:"Masks",value:0
+          },
+          {
+            key:"Isolation",value:0
+          },
+          {
+            key:"Isolation-Delay",value:"Null"
+          },
+          {
+            key:"Isolation-Rate",value:"Null"
+          },
+          {
+            key:"Masked-Type",value:"Null"
+          },
+          {
+            key:"Vaccines",value:0
+          },
+          {
+            key:"Stay-Home",value:0
+          },
+          {
+            key:"School-Close",value:0
+          },
+          {
+            key:"School-Close-Duration",value:"Null"
+          },
+        ],
       }
     },
     mounted() {
+      var idx = 0;
       for (var key in this.RunMeta) {
         var item = this.RunMeta[key];
+        if(idx<5){
         this.MetaData.push({key: key, value:item})
+        }
+        else{
+          this.RunData.push({key: key, value:item})
+        }
+        idx = idx+1;
 }
       
     }
